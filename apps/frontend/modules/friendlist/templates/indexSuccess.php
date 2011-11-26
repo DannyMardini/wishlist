ea17bdf03f82346647a8bc7108ecb42875c2669b
Daniel Mardini <dannymardini@gmail.com>
2011-11-25 23:16:45 -0800

    Theming the friendlist.

--- apps/frontend/modules/friendlist/templates/indexSuccess.php
+++ apps/frontend/modules/friendlist/templates/indexSuccess.php
@@ -12,9 +12,10 @@ use_stylesheet("friendlist.css");
 
 <div id="friendlist">
     <p>Friends:</p>
-    <ul id="selectable">
+    <ul id="selectable" class="ui-selectable">
         <?php foreach ($friends as $i => $friend): ?>
-        <li class="<?php echo fmod($i, 2) ? "odd" : "even" ?>">
+        <!--<li class="<?php echo fmod($i, 2) ? "odd" : "even" ?>">-->
+        <li class="ui-selectee ui-widget-content">
             <?php echo $friend ?>
         </li>
         <?php endforeach;?>
