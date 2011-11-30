64c36df627dbcb93bcde507566ce9d83d8545414
Daniel Mardini <dannymardini@gmail.com>
2011-11-29 19:06:20 -0800

    Making changes to showSuccess.php

--- apps/frontend/modules/user/templates/showSuccess.php
+++ apps/frontend/modules/user/templates/showSuccess.php
@@ -11,41 +11,6 @@
     <p>Member since: <?php echo $wishlist_user->getCreatedAt(); ?></p>
 </div>
 
-<table>
-  <tbody>
-    <tr>
-      <th>Name:</th>
-      <td><?php echo $wishlist_user->getName() ?></td>
-    </tr>
-    <tr>
-      <th>Gender:</th>
-      <td><?php echo $wishlist_user->getGender() ?></td>
-    </tr>
-    <tr>
-      <th>Birthdate:</th>
-      <td><?php echo $wishlist_user->getBirthdate() ?></td>
-    </tr>
-    <tr>
-      <th>Email:</th>
-      <td><?php echo $wishlist_user->getEmail() ?></td>
-    </tr>
-    <tr>
-      <th>Wishlistuser:</th>
-      <td><?php echo $wishlist_user->getWishlistuserId() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $wishlist_user->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $wishlist_user->getUpdatedAt() ?></td>
-    </tr>
-  </tbody>
-</table>
-
-<hr />
-
 <a href="<?php echo url_for('user/edit?wishlistuser_id='.$wishlist_user->getWishlistuserId()) ?>">Edit</a>
 &nbsp;
 <a href="<?php echo url_for('user/index') ?>">List</a>
