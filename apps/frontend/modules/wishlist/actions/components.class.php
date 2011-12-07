ee0b4804e24d65602767300a2ca050a24305e288
Andrea <andreacoba@gmail.com>
2011-12-06 23:54:01 -0800

    more stuff

--- apps/frontend/modules/wishlist/actions/components.class.php
+++ apps/frontend/modules/wishlist/actions/components.class.php
@@ -4,14 +4,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-class newsComponents extends sfComponents
-{
-  public function executeHeadlines()
-  {
-    $c = new Criteria();
-    $c->addDescendingOrderByColumn(NewsPeer::PUBLISHED_AT);
-    $c->setLimit(5);
-    $this->news = NewsPeer::doSelect($c);
-  }
+class wishlistComponents extends sfComponents
+{ 
+  public function executeShowWishlist(sfWebRequest $request)
+    {
+        $this->wishlist_user = Doctrine_Core::getTable('WishlistUser')->find(array($request->getParameter('wishlistuser_id')));
+        $this->forward404Unless($this->wishlist_user);
+
+        $this->wishlist_items = $this->wishlist_user->getWishlistItems();
+    }
 }
 
