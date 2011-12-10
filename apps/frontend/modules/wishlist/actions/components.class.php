54abe5f9500d425f8b1d674c3f500f52b2a0c9df
Andrea <andreacoba@gmail.com>
2011-12-09 22:48:17 -0800

    homepage uses the wishlist component but still needs the CSS to work

--- apps/frontend/modules/wishlist/actions/components.class.php
+++ apps/frontend/modules/wishlist/actions/components.class.php
@@ -8,6 +8,10 @@
 class wishlistComponents extends sfComponents
 { 
   public function executeShowWishlist()
-    {
-    }
+  {
+    $this->wishlist_user = Doctrine_Core::getTable('WishlistUser')->find($this->wishlistuser_id);
+    //$this->forward404Unless($this->wishlist_user);
+
+    $this->wishlist_items = $this->wishlist_user->getWishlistItems();
+  }
 }
