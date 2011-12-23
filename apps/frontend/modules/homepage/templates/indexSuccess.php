6d9d3d35cc095e0c7d1284903a5100d97066b3aa
Andrea <andreacoba@gmail.com>
2011-12-23 13:22:17 -0800

    changes...can't really remember what they were for

--- apps/frontend/modules/homepage/templates/indexSuccess.php
+++ apps/frontend/modules/homepage/templates/indexSuccess.php
@@ -9,7 +9,6 @@ use_javascript('/js/wishlist.js');
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>    
   
-<form>
 <input id="username" type="hidden" value="<?php echo $username ?>"/>  
 <input id="id" type="hidden" value="<?php echo $id ?>"/>  
 <div id="header">
@@ -35,63 +34,63 @@ use_javascript('/js/wishlist.js');
 </div>
 <div id="headerDivider"></div>
 
-<div id="wishlistComponent">
-    <div><h1>My Wishlist</h1></div>
-    <?php include_component('wishlist', 'showWishlist', array('wishlistuser_id' => $id)); ?>
-</div>
-<div id="newsComponent">
-    <div id="newsInnerComponent">
-        <div id="2" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
-            <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
-        </div>
-        <div id="1" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
-            <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
-        </div>
-        <div id="2" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
-            <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
-        </div>
-        <div id="1" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
-            <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
-        </div>
-        <div id="2" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
-            <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
-        </div>
-        <div id="1" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
-            <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
-        </div>
-        <div id="2" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
-            <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
-        </div>
-        <div id="1" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
-            <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
-        </div>
-        <div id="2" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
-            <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
-        </div>
-        <div id="1" class="friendUpdate">
-            <div class="image"></div>
-            <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
-            <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
+<div id="content">
+    <div id="wishlistComponent">
+        <div><h1>My Wishlist</h1></div>
+        <?php include_component('wishlist', 'showWishlist', array('wishlistuser_id' => $id)); ?>
+    </div>
+    <div id="newsComponent">
+        <div id="newsInnerComponent">
+            <div id="2" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
+                <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
+            </div>
+            <div id="1" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
+                <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
+            </div>
+            <div id="2" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
+                <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
+            </div>
+            <div id="1" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
+                <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
+            </div>
+            <div id="2" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
+                <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
+            </div>
+            <div id="1" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
+                <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
+            </div>
+            <div id="2" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
+                <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
+            </div>
+            <div id="1" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
+                <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
+            </div>
+            <div id="2" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Andrea Coba -- 12/11/2011 @ 7:00pm</div>
+                <div class="info">Added the <a href="http://www.google.com">ipad2</a> to her wishlist</div>
+            </div>
+            <div id="1" class="friendUpdate">
+                <div class="image"></div>
+                <div class="name">Daniel Mardini -- Birthday coming up on 06/11/2012</div>
+                <div class="info">Check <a href="http://www.google.com">Danny's wishlist</a> for gift ideas</div>
+            </div>
         </div>
     </div>
 </div>
-<div id="footer">footer</div>
-</form>
