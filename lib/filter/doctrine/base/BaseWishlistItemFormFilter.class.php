1a649d7ca215d6919892f570a9208d517e33db39
Daniel Mardini <dannymardini@gmail.com>
2012-01-11 23:50:49 -0800

    I can add new wishlist items however I have to refresh the page to see the change.... must fix this...

--- lib/filter/doctrine/base/BaseWishlistItemFormFilter.class.php
+++ lib/filter/doctrine/base/BaseWishlistItemFormFilter.class.php
@@ -15,6 +15,7 @@ abstract class BaseWishlistItemFormFilter extends BaseFormFilterDoctrine
     $this->setWidgets(array(
       'name'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'price'      => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'link'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'is_public'  => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
       'user_id'    => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('WishlistUser'), 'add_empty' => true)),
       'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
@@ -24,6 +25,7 @@ abstract class BaseWishlistItemFormFilter extends BaseFormFilterDoctrine
     $this->setValidators(array(
       'name'       => new sfValidatorPass(array('required' => false)),
       'price'      => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
+      'link'       => new sfValidatorPass(array('required' => false)),
       'is_public'  => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
       'user_id'    => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('WishlistUser'), 'column' => 'wishlistuser_id')),
       'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
@@ -50,6 +52,7 @@ abstract class BaseWishlistItemFormFilter extends BaseFormFilterDoctrine
       'id'         => 'Number',
       'name'       => 'Text',
       'price'      => 'Number',
+      'link'       => 'Text',
       'is_public'  => 'Boolean',
       'user_id'    => 'ForeignKey',
       'created_at' => 'Date',
