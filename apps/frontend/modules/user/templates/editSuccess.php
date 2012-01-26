8f6247b6b77328d37867a2894915700b81548351
Daniel Mardini <dannymardini@gmail.com>
2012-01-25 22:40:43 -0800

    Removing files in prep for Symfony2

--- apps/frontend/modules/user/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>Edit Wishlist user</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
--- refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-php symfony doctrine:build --all --and-load --no-confirmation
-php symfony cache:clear
-echo "Successfully rebuilt DB and cleared the cache." 
-exit
