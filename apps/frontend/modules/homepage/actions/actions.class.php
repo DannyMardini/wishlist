c1f81748cd963866533bb8033ddc429ef72225d7
Andrea <andreacoba@gmail.com>
2012-01-25 21:49:55 -0800

    stuff

--- apps/frontend/modules/homepage/actions/actions.class.php
+++ apps/frontend/modules/homepage/actions/actions.class.php
@@ -8,6 +8,9 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
+include 'util/time.php';
+use util\Time;
+
 class homepageActions extends sfActions
 {
  /**
--- lib/model/util/time.php
+++ lib/model/util/time.php
@@ -3,40 +3,41 @@ namespace util;
 
 class Time
 {
-    public static function getFormattedTimestamp()
+    public static function getFormattedTimestamp($strDateTime)
     {
-        $submittedDateTime = strtotime($this->getDatetime());
-        $thisYear = date('Y') == date('Y', $submittedDateTime);
+        //$submittedDateTime = strtotime($this->getDatetime());
+        $dateTime = strtotime($strDateTime);
+        $thisYear = date('Y') == date('Y', $dateTime);
 
         if ( $thisYear > 0 )
         {
-            $thisWeek = date('W') == date('W', $submittedDateTime);
+            $thisWeek = date('W') == date('W', $dateTime);
             if( $thisWeek > 0 )
             {
-                $yesterday = date('j', $submittedDateTime) == (date('j')-1);
-                $today = date('j') == date('j', $submittedDateTime);
+                $yesterday = date('j', $dateTime) == (date('j')-1);
+                $today = date('j') == date('j', $dateTime);
 
                 if( $yesterday > 0 )
                 {
-                    return " Yesterday @ ".date("g:i a", $submittedDateTime);
+                    return " Yesterday @ ".date("g:i a", $dateTime);
                 }
                 else if( $today > 0 )
                 {
-                    return "Today @".date(" g:i a", $submittedDateTime);
+                    return "Today @".date(" g:i a", $dateTime);
                 }
                 else
                 {
-                    return date(  "l \@ g:i a", $submittedDateTime);
+                    return date(  "l \@ g:i a", $dateTime);
                 }
             }
             else
             {
-                return date(  "F jS \@ g:i a", $submittedDateTime);
+                return date(  "F jS \@ g:i a", $dateTime);
             }
         }
         else
         {
-            $seconds = time() - $submittedDateTime;
+            $seconds = time() - $dateTime;
             $days = round($seconds / (24*60*60));
 
             $yesterday = $days <= 1;
@@ -44,15 +45,15 @@ class Time
 
             if($yesterday > 0)
             {
-                return " yesterday @ ".date("g:i a", $submittedDateTime);
+                return " yesterday @ ".date("g:i a", $dateTime);
             }
             else if($thisWeek > 0)
             {
-                return date(  "l \@ g:i a", $submittedDateTime);
+                return date(  "l \@ g:i a", $dateTime);
             }
             else
             {
-                return date(  "F jS, Y, g:i a", $submittedDateTime);
+                return date(  "F jS, Y, g:i a", $dateTime);
             }
         }
     }
