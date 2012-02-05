8ba042b4f547183988bd57a79e80e5be9905da72
Andrea <andreacoba@gmail.com>
2012-02-04 19:35:15 -0800

    homepage migration in progress...

---
+++ src/Wishlist/HomepageBundle/Controller/DefaultController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wishlist\HomepageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction()
+    {
+        $request = Request::createFromGlobals()->request;        
+        
+        
+        //return $this->render('WishlistHomepageBundle:Default:index.html.twig', array('name' => $name));
+        $email = $request->get('email_addr');   
+        $email = 'andreacoba@gmail.com';
+        
+        if(!$email){
+            throw $this->createNotFoundException ('500 Internal server error. Please go to wishlist.com and sign in.');
+        }
+
+        $pass = $request->get('password');
+        
+        //return new Response('<html><body>Hello test test!</body></html>');
+        
+        return $this->render('WishlistHomepageBundle:Default:index.html.php', array('email' => $email));
+
+        
+        try {
+            /*
+            $this->user = WishlistUserTable::getInstance()->getUserWithEmail($email);
+
+            if ($this->user->getPassword() != $pass)
+            {
+                throw new Exception('Incorrect password');
+            }
+
+            $_SESSION['user'] = $this->user->getEmail();
+            $this->friendUpdates = WishlistUpdateTable::getInstance()->GetFriendsUpdates($this->user->getWishlistuser_id());
+             * 
+             */
+        }catch(Exception $e){
+            $e->getTrace();
+        }                         
+    }
+}
---
+++ src/Wishlist/HomepageBundle/Resources/views/Default/index.html.php
@@ -0,0 +1,67 @@
+<?php
+use_stylesheet('homePage.css');
+use_stylesheet('/css/wishlist.css');
+use_javascript('/js/wishlist.js');
+use_javascript('/js/homepage.js');
+use_javascript('/js/common.js');
+?>
+  
+<input id="username" type="hidden" value="<?php echo $user->getFirstname() ?>"/>
+<input id="id" type="hidden" value="<?php echo $user->getWishlistuserId() ?>"/>
+
+<div class="sideBar" >
+    <div id="wishlistComponent">
+        <div><h1>My Wishlist</h1></div>
+        <div id="wishlist">
+            <?php include_component('wishlist', 'showWishlist', array('wishlistuser_id' => $user->getWishlistuserId())); ?>
+        </div>
+    </div>
+
+    <div id="upcomingEvents">
+        <div><h1>Upcoming Events</h1></div>
+        <div id="events">
+            <div><h2>Birthdays</h2></div>
+            <div id="birthdays"></div>
+            <div><h2>Anniversaries</h2></div>
+            <div id="anniversaries"></div>
+        </div>
+    </div>
+
+</div>
+
+<div id="updatesComponent">
+    <div id="updatesInnerComponent">
+
+        <?php
+        foreach ($friendUpdates as $update) {
+
+                $message = $update->getMessage(ESC_RAW);
+                $name = "<a href='user/".$update->getUserId()."/' >".$update[concat]."</a>";
+                $timestamp = " -- ".$update->getFormattedTimestamp();
+
+            ?>
+
+        <div class="Update">
+            <div class="image"><img src="/images/default_avatar.gif" alt="Smiley face" height="42" width="42" /></div>
+            <div class="name"><?php echo $name ?></div>
+            <div class="message"><?php echo $message ?></div>
+            <div class="timestamp"><?php echo $timestamp ?></div>
+        </div>
+
+        <?php } ?>
+    </div>
+</div>
+
+
+<div id="itemDialog" title="Wish" >        
+    <label class="label">Name: </label>
+    <div id="name"></div>
+    <label class="label">Price: </label>
+    <div id="price"></div>
+    <label class="label">Quantity: </label>
+    <div id="quantity">2</div>
+    <label class="label">Website: </label>
+    <div id="link"></div>
+    <label class="label">Comment: </label>
+    <div id="comment"</div>
+</div>
