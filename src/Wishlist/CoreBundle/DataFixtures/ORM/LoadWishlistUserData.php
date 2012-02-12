8f649b151e5499dc1043964784a7f6edd81313f6
Daniel Mardini <dannymardini@gmail.com>
2012-02-11 22:40:41 -0800

    Finally I injected a fixture

--- src/Wishlist/CoreBundle/DataFixtures/ORM/LoadWishlistUserData.php
+++ src/Wishlist/CoreBundle/DataFixtures/ORM/LoadWishlistUserData.php
@@ -4,11 +4,24 @@ namespace Wishlist\CoreBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Wishlist\CoreBundle\Entity\WishlistUser;
+use \DateTime;
 
 class LoadWishlistUserData implements FixtureInterface
 {
-    public function load($manager)
+    public function load(\Doctrine\Common\Persistence\ObjectManager $manager)
     {
+        $birthdate = new DateTime("06/11/1986");
+        
+        $user = new WishlistUser();
+        $user->setBirthdate($birthdate);
+        $user->setEmail("hoohaw@email.com");
+        $user->setGender(1);
+        $user->setFirstname("danny");
+        $user->setLastname("Mardini");
+        $user->setPassword("hoohaw");
+        
+        $manager->persist($user);
+        $manager->flush();
     }
 }
 
