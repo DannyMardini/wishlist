ee19c8eb2cec2e19cdc74ba6c8cf07937987037d
Daniel Mardini <dannymardini@gmail.com>
2012-02-11 22:10:23 -0800

    Adding fixtures file.

---
+++ src/Wishlist/CoreBundle/DataFixtures/ORM/LoadWishlistUserData.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wishlist\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Wishlist\CoreBundle\Entity\WishlistUser;
+
+class LoadWishlistUserData implements FixtureInterface
+{
+    public function load($manager)
+    {
+    }
+}
+
+?>
