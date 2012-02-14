d7554f4f668a7d69af65f0baa2d8c2ae8075f5c1
Daniel Mardini <dannymardini@gmail.com>
2012-02-13 23:04:55 -0800

    Adding convenience function in LoadWishlistUserData.

--- src/Wishlist/CoreBundle/DataFixtures/ORM/LoadWishlistUserData.php
+++ src/Wishlist/CoreBundle/DataFixtures/ORM/LoadWishlistUserData.php
@@ -2,6 +2,7 @@
 
 namespace Wishlist\CoreBundle\DataFixtures\ORM;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Wishlist\CoreBundle\Entity\WishlistUser;
 use Wishlist\CoreBundle\Repository\EnumRepository;
@@ -9,24 +10,28 @@ use \DateTime;
 
 class LoadWishlistUserData implements FixtureInterface
 {
-    public function load(\Doctrine\Common\Persistence\ObjectManager $manager)
+    public function load(ObjectManager $manager)
     {
         $birthdate = new DateTime("06/11/1986");
-        
         $enumRepo = $manager->getRepository('WishlistCoreBundle:Enum');
-        
         $maleEnum = $enumRepo->findOneByName('male');
         
-        $user = new WishlistUser();
-        $user->setBirthdate($birthdate);
-        $user->setEmail("hoohaw@email.com");
-        $user->setGender($maleEnum);
-        $user->setFirstname("danny");
-        $user->setLastname("Mardini");
-        $user->setPassword("hoohaw");
+        addUser($manager, "Danny", "Mardini", $birthdate, "hoohaw@gmail.com", $maleEnum, "hoohaw");
         
-        $manager->persist($user);
         $manager->flush();
+    }
+
+    public function addUser(ObjectManager $manager, String $firstname, String $lastname, DateTime $birthdate, String $email, Enum $gender, String $password)
+    {
+        $user = new WishlistUser();
+        
+        $user->setBirthdate($birthdate);
+        $user->setEmail($email);
+        $user->setGender($gender);
+        $user->setFirstname($firstname);
+        $user->setLastname($lastname);
+        $user->setPassword($password);
+        $manager->persist($user);
     }
 }
 
