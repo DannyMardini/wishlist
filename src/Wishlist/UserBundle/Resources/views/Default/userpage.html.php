acb715062623c73b33f9360ddbd4b73351698ed6
Daniel Mardini <dannymardini@gmail.com>
2012-04-04 12:38:46 -0700

    Fixing the userpage so it correctly links to the friend page.

--- src/Wishlist/UserBundle/Resources/views/Default/userpage.html.php
+++ src/Wishlist/UserBundle/Resources/views/Default/userpage.html.php
@@ -30,7 +30,7 @@ $selfWishlist = ($wishlist_user->getWishlistUserId() == $loggedInUserId)? true:f
         <p><label>Birthday <?php echo $wishlist_user->getBirthdate()->format('M d') ?></label></p>
         <p><label><?php echo $wishlist_user->getGender() ?></label></p>
         <p><label><?php echo $wishlist_user->getEmail() ?></label></p>
-        <p><label><a href="<?php echo $view['router']->generate('WishlistListBundle_friendlist', array('user_id' => $wishlist_user->getWishlistuserId())) ?>">Friends</a> <?php echo count($wishlist_user->getFriendships()) ?></label></p>
+        <p><label><a href="<?php echo $view['router']->generate('WishlistUserBundle_friendlist', array('user_id' => $wishlist_user->getWishlistuserId())) ?>">Friends</a> <?php echo count($wishlist_user->getFriendships()) ?></label></p>
     </div>
 </div>
 
