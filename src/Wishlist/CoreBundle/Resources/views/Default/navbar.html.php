1e2864f237543933e5818f904c64909c96f4d40b
Daniel Mardini <dannymardini@gmail.com>
2012-04-12 23:45:09 -0700

    Adding new navigation bar button and making cosmetic changes.

--- src/Wishlist/CoreBundle/Resources/views/Default/navbar.html.php
+++ src/Wishlist/CoreBundle/Resources/views/Default/navbar.html.php
@@ -5,21 +5,18 @@
     </div>
     <div id="linksContainer">
         <ul id="navigation">
+            <!--
+                WOW, html and css suck... are you kidding me?? inline-block is the more correct approach here...
+                HOWEVER if there is a line break between successive li's then there will be a small gap between them 
+                in the actual presentation... html and css are fucking broken...
+            -->
             <li>
                 <img class="tinyProfile" src="<?php echo "/images/user/".$user->getWishlistuserId()."/profile.jpg" ?>"/>
-            </li>
-            
-            <li class="navLink">
+            </li><li class="navLink">
                 <a href="<?php echo $view['router']->generate('WishlistUserBundle_homepage')?>"><?php echo $user->getFirstName() ?></a>
-            </li>
-            
-            <li class="navLink">
+            </li><li class="navLink">
                 <a href="<?php echo $view['router']->generate('WishlistUserBundle_friendlist', array('user_id' => $user->getWishlistuserId())) ?>">Friends</a>
-            </li>
-            
-<!--            <li class="navLink">
-                <a href="#">Settings</a>
-            </li>-->
+            </li><li class="navLink"><span class="ui-icon ui-icon-carat-1-s"></span></li>
         </ul>
     </div>
 </div>
