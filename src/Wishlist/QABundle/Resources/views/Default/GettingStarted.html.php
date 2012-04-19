8a25258135170dc22a948d70d61a02fd91edd00b
Andrea <andreacoba@gmail.com>
2012-04-19 13:17:40 -0700

    changed the help section to use ajax load need to see if i can get the jquery to work with the ajax loaded html without having to include the js in each separate html page

--- src/Wishlist/QABundle/Resources/views/Default/GettingStarted.html.php
+++ src/Wishlist/QABundle/Resources/views/Default/GettingStarted.html.php
@@ -1,32 +1,15 @@
-<html>
-    <head>
-        <link href="/css/help.css" rel="stylesheet" type="text/css" />
-            <link href="/css/navBar.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <?php $view->extend('::navBar.html.php') ?>
-        <header>How can we help you?</header>
-        <div class="content">
-            <div class="contentMenu">                
-                <?php echo $view->render('WishlistQABundle:Default:QAMenu.html.php', array('selectedOptionIndex' => 1)); ?>
-            </div>
-            <div class="contentInfo">
-                <div class="contentInfoTopic">What is Wishlist?</div>
-                <div class="contentInfoDetails">
-                    Wishlist lets you keep track of all of your wishes in one location and shares your list with friends! <br />
-                    Your friends can see when your birthday or anniversary is coming up and get you a present from your list without you knowing!<br /><br />
-                    Our Goal is to make life easier by providing a central location where everyone can bookmark their wishes and their friends can easily see what they want!<br /><br />
-                </div>
-                <div class="contentInfoTopic">Account Settings</div>
-                <div class="contentInfoDetails">
-                    To DO
-                </div>                  
-                <div class="contentInfoTopic">Linking to your website</div>
-                <div class="contentInfoDetails">
-                    To DO
-                </div>                  
-            </div>
-        </div>
-        <footer></footer>
-    </body>    
-</html>
+
+<div class="contentInfoTopic">What is Wishlist?</div>
+<div class="contentInfoDetails">
+    Wishlist lets you keep track of all of your wishes in one location and shares your list with friends! <br />
+    Your friends can see when your birthday or anniversary is coming up and get you a present from your list without you knowing!<br /><br />
+    Our Goal is to make life easier by providing a central location where everyone can bookmark their wishes and their friends can easily see what they want!<br /><br />
+</div>
+<div class="contentInfoTopic">Account Settings</div>
+<div class="contentInfoDetails">
+    To DO
+</div>                  
+<div class="contentInfoTopic">Linking to your website</div>
+<div class="contentInfoDetails">
+    To DO
+</div>                  
