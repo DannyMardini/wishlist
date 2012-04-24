763a6d72e17f6e5cf1c266f69bfed713f01eeb11
Andrea <andreacoba@gmail.com>
2012-04-23 23:17:39 -0700

    finished downloading the google web fonts so we do not have to access the google site each time.

--- app/Resources/views/navBar.html.php
+++ app/Resources/views/navBar.html.php
@@ -7,16 +7,13 @@
 ?>
 <html>
     <head>
-        <link href='http://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet' />
-        <link href='http://fonts.googleapis.com/css?family=Gruppo' rel='stylesheet' />
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <script type="text/javascript" src="/js/jquery-1.7.1.js"></script>
         <script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min"></script>
+        <script type="text/javascript" src="/js/navBar.js"></script>        
         <link href="/css/custom-theme/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
-        <link href="/css/main.css" rel="stylesheet" type="text/css" />
-        
-        <script type="text/javascript" src="/js/navBar.js"></script>
-        <link href="/css/navBar.css" rel="stylesheet" type="text/css" />
+        <link href="/css/main.css" rel="stylesheet" type="text/css" />                
+        <link href="/css/navBar.css" rel="stylesheet" type="text/css" />        
     </head>
     <body>
         <?php echo $view['actions']->render('WishlistCoreBundle:Default:navBar'); ?>
