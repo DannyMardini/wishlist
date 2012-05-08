dbc81be793e4c25f5c05f172cdbcdf18045eb668
Andrea <andreacoba@gmail.com>
2012-05-07 22:50:18 -0700

    changed Getting Started description

--- src/Wishlist/QABundle/Resources/views/Default/GettingStarted.html.php
+++ src/Wishlist/QABundle/Resources/views/Default/GettingStarted.html.php
@@ -1,15 +1,15 @@
 
 <div class="contentInfoTopic">What is Wishlist?</div>
 <div class="contentInfoDetails">
-    Wishlist lets you keep track of all of your wishes in one location and shares your list with friends! <br />
-    Your friends can see when your birthday or anniversary is coming up and get you a present from your list without you knowing!<br /><br />
-    Our Goal is to make life easier by providing a central location where everyone can bookmark their wishes and their friends can easily see what they want!<br /><br />
+    Are you tired of getting that ugly old sweater from your aunt on Christmas? Or being clueless about what to get your boyfriend for his birthday? What about the nightmare it was when you forgot your sisters birthday!<br />
+    You've come to the right place! We guarantee you that Wishlist will make all these problems seem like they were just a bad dream!<br /><br />
+    
+    Wishlist helps you keep track of your wishes and share them with friends! <br />
+    Your friends can see when your birthday, anniversary or any other life event is coming up and grant you a wish from your list without you knowing.<br />
+    You also have access to a shopping list where you can keep track of wishes you plan on granting to your friends. Once a gift is given, it is taken off your shopping list and your friends wishlist.<br /><br />
+    <b><u>Our Goal:</u></b><i> to make your life easier by providing a central location where you can easily bookmark and keep track of your wishes; making it easy for your friends to see and get you just what you want!</i><br /><br />
 </div>
 <div class="contentInfoTopic">Account Settings</div>
 <div class="contentInfoDetails">
-    To DO<br />    yyyy<br />    yrdy 
-</div>                  
-<div class="contentInfoTopic">Linking to your website</div>
-<div class="contentInfoDetails">
-    To DO<br />    test<br />    test
-</div>                  
+    To DO<br />
+</div>
