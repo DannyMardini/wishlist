778392f3565d734f96acd949008de2f8e5e34566
Daniel Mardini <dannymardini@gmail.com>
2012-05-17 23:09:50 -0700

    Changing function getFriendEvents to getUserEvents.

--- src/Wishlist/UserBundle/Controller/DefaultController.php
+++ src/Wishlist/UserBundle/Controller/DefaultController.php
@@ -32,7 +32,7 @@ class DefaultController extends Controller
 
         try {
             $friendUpdates =  $updateRepo->getFriendsUpdates($user->getWishlistuserId());
-            $friendEvents = $eventRepo->getFriendsEvents($user->getWishlistuserId());
+            $friendEvents = $eventRepo->getUserEvents($user->getWishlistuserId());
         }catch(Exception $e){
             $e->getTrace();
         }
