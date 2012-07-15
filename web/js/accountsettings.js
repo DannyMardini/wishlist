313d77eef346241db910c7b0fb1ef3c98418639b
Andrea <andreacoba@gmail.com>
2012-07-15 16:28:24 -0700

    the js has been added so that when a user adds a new event it shows up under "my events"

--- web/js/accountsettings.js
+++ web/js/accountsettings.js
@@ -88,6 +88,7 @@
     {
         newEventCounter++;
         var eventId = "newEvent"+newEventCounter;
+        var jqueryEventId = "#" + eventId;
         
         $("<div/>", {
             "class": "flexbox",
@@ -97,16 +98,42 @@
         $('<input />', {
             type: 'text',
             name: 'event_name',
+            "value": $('#newEventname').val(),
             "class": "eventname",
             "placeholder": "name"
-        }).appendTo('#'+eventId);
+        }).appendTo(jqueryEventId);
+        
+        $('<input />', {
+            type: 'text',
+            "class": "datepicker",
+            "value": $('#newDatepicker').val(),
+            "placeholder": "mm/dd/yyyy"
+        }).appendTo(jqueryEventId);
+        
+        var select = $("<select><option value='-1'>--Type--</option><option value='1'>Birthday</option>" + 
+            "<option value='2'>Anniversary</option></select>");
+        $('option[value='+ $('#newEventType :selected').val() +']',select).attr('selected', 'selected');
+        $(select).appendTo(jqueryEventId);  
+        
+        $("<img />", {
+            "class": "buttonClass",
+            "src": "/images/remove_icon.jpeg", 
+            "alt": "Remove this event"
+        }).appendTo(jqueryEventId);
+        
+        // clear the entries previously input
+        $('#newEventname').val('');
+        $('#newDatepicker').val('');
+        $('#newEventType :selected').removeAttr('selected');
+        $('option[value=-1]','#newEventType').attr('selected', 'selected');
+        
         
     }
     
     function validateEventInputs()
     {
         return ($('#newEventname').val() != "" && $('#newDatepicker').val() != ""
-        && $("#newEventType option:selected").length > 0);
+        && $("#newEventType option:selected").val()!=-1);
     }
     
     function redirectToUserPage()
