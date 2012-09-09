7fb4d4c5d2b50543569b1f149f203ca956e15910
Andrea <andreacoba@gmail.com>
2012-09-09 14:23:56 -0700

    changes

--- app/Resources/views/main.html.php
+++ app/Resources/views/main.html.php
@@ -4,8 +4,8 @@
         <link href='http://fonts.googleapis.com/css?family=Gruppo' rel='stylesheet' />
         <link rel="shortcut icon" href="/images/favicon.ico">
         <script type="text/javascript" src="/js/common.js"></script>
-        <script type="text/javascript" src="/js/jquery-1.8.0.min.js"></script>
-        <script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min"></script>
+        <script type="text/javascript" src="/js/jquery-1.8.1.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min.js"></script>
         <link href="/css/custom-theme/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
 <!--        <link href="/css/main.css" rel="stylesheet" type="text/css" />-->
     </head>
--- src/Wishlist/UserBundle/Resources/views/Default/lifeEventsManager.html.php
+++ src/Wishlist/UserBundle/Resources/views/Default/lifeEventsManager.html.php
@@ -8,8 +8,8 @@
     <body>
         <div class="eventsHeader">
             <label>Life Events</label>
-            <button title="add new life event" id="addLifeEventButton"></button>
-            <button title="remove selected life events" id="removeLifeEventButton"></button>
+            <button title="add event" id="addLifeEventButton"></button>
+            <button title="remove event(s)" id="removeLifeEventButton"></button>
             <button title="save changes" id="saveLifeEventButton"></button>
         </div>
         <hr size="1" width="90%" color="grey">
@@ -28,8 +28,8 @@
                     <div class="Event"> 
                         <div class="checkbox"><input type="checkbox"></div>
                         <div class="image" title="<?php echo $eventName ?>"><img src="<?php echo $eventImage ?>" height="30" width="30" /></div>
-                        <div class="name" title ="<?php echo $eventName ?>"><?php echo $name ?></div>
-                        <div class="message" title="<?php echo $eventName ?>"><?php echo $timestamp ?></div>
+                        <div class="name" title ="<?php echo $eventName ?>"><?php echo $eventName ?></div>
+                        <div class="timestamp" title="<?php echo $eventName ?>"><?php echo $timestamp ?></div>
                     </div>
            <?php 
 
@@ -41,28 +41,5 @@
             }
            ?>            
         </div>
-        
-<!--        <div class="lifeEventDiv right_content">
-            <div class="right_innercontent">
-                <div id="new_life_event_div" class="newEventDiv">                    
-                    <label>Add New Event: </label>
-                    <div class="flexbox">
-                        <input type="text" id="newEventname" class="eventname" name="event_name" placeholder="name">
-                        <input type="text" id="newDatepicker" class="datepicker" placeholder="mm/dd/yyyy">
-                        <select id="newEventType">
-                        <option value="-1">--Type--</option>
-                        <option value="1">Birthday</option>
-                        <option value="2">Anniversary</option>                
-                        </select>
-                        <img class="buttonClass" id="addEventButton" src="/images/plus_icon.jpeg" alt="Add a new life event" />
-                    </div>                    
-                </div>
-
-                <div id="saved_life_events_div" class="newEventDiv" style="margin-top:20px">
-                    <label> My Events: </label>                    
-                </div>
-            </div>
-        </div>-->
-        
     </body>
 </html>
