0d891c4279fc10fe9232ac06f35aff4d0309f80c
Daniel Mardini <dannymardini@gmail.com>
2012-09-20 22:59:42 -0700

    Fixing jquery stuff.

--- src/Wishlist/FrontpageBundle/Resources/views/Default/indexSuccess.html.php
+++ src/Wishlist/FrontpageBundle/Resources/views/Default/indexSuccess.html.php
@@ -1,9 +1,11 @@
 <html>
     <head>
         <link rel="shortcut icon" href="/images/favicon.ico">
-        <script type="text/javascript" src="/js/jquery-1.8.0.min.js"></script>
-        <script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min"></script>
-        <link href="/css/custom-theme/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
+
+        <script type="text/javascript" src="/js/jquery-1.8.2.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui-1.8.23.custom.min.js"></script>
+        <link href="/css/black-tie/jquery-ui-1.8.23.custom.css" rel="stylesheet" />
+
         <link href="/css/main.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
