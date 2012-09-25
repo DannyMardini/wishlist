80b0995070adab8302af88c551d557b70d30de85
Andrea <andreacoba@gmail.com>
2012-09-24 23:02:21 -0700

    enable remove button only if an event has been selected

--- web/js/lifeEventsManager.js
+++ web/js/lifeEventsManager.js
@@ -14,11 +14,27 @@ $(document).ready(function(){
             text: false
     });
     
+    $('#removeLifeEventButton').attr('disabled', 'disabled'); // disabled by default
+    
     $('#saveLifeEventButton').button({
             icons: {
                 primary: "ui-icon-disk"
             },
             text: false
+    });
+    
+    $('.Event input:checkbox').click(function(){
+        
+        // check if any checkboxes are selected
+        var arrChecked = $('input:checked',$('.Event'));
+        if(arrChecked.length > 0)
+        {
+            $('#removeLifeEventButton').removeAttr('disabled');
+        }
+        else
+        {
+            $('#removeLifeEventButton').attr('disabled', 'disabled');
+        }
     });
     
 });
