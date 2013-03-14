bd69ca64563d22b2e352c0579e5d1452a5b485be
Andrea <andreacoba@gmail.com>
2013-03-13 21:29:41 -0700

    removed commented out code

--- src/Wishlist/CoreBundle/Repository/WishlistItemRepository.php
+++ src/Wishlist/CoreBundle/Repository/WishlistItemRepository.php
@@ -46,45 +46,6 @@ class WishlistItemRepository extends EntityRepository
         $itemInDatabase = $q->getOneOrNullResult();  
         return $itemInDatabase;
     } 
-//    
-//    public function addItem( $name, $price, $link, $isPublic, $comment, $quantity, WishlistUser $wishlistUser)
-//    {
-//        $newItem = new Item();
-//        $newItem->setName($name);
-//        $newItem->setPrice($price);
-//        $newItem->setLink($link);
-//        $newItem->setIsPublic($isPublic);
-//        $newItem->setComment($comment);
-//        $newItem->setQuantity($quantity);
-//        $newItem->setWishlistUser($wishlistUser);
-//        
-//        
-//        // check if the item already exists for this user
-//        $itemExists = $this->checkItemExists($newItem);
-//        
-//        if(!$itemExists){
-//            $this->getEntityManager()->persist($newItem);
-//            $this->getEntityManager()->flush();
-//
-//            $updateRepo = $this->getEntityManager()->getRepository('WishlistCoreBundle:WishlistUpdate');
-//            $updateRepo->addNewItem($wishlistUser, $newItem);
-//        }
-//    }
-//    
-//    public function checkItemExists($item)
-//    {
-//       $em = $this->getEntityManager();       
-//        
-//        $q = $em->createQuery('
-//            SELECT i
-//            FROM WishlistCoreBundle:Item i
-//            LEFT JOIN i.wishlistUser usr
-//            WHERE i.name = :itemName AND usr.wishlistuser_id = :userId')
-//                ->setParameters(array('itemName' => $item->getName(), 'userId' => $item->getWishlistUser()->getWishlistuserId()));
-//                      
-//        $itemInDatabase = $q->getOneOrNullResult();  
-//        return $itemInDatabase;
-//    }
     
     public function deleteItem( $deletedItemName, WishlistUser $wishlistUser)
     {
