b9a43571e7e8bba28c7b4270d16cd5693a822b52
Andrea <andreacoba@gmail.com>
2013-05-04 16:08:17 -0700

    removing commented out code

--- src/Wishlist/UserBundle/Resources/views/Giftbox/shoppinglist.html.php
+++ src/Wishlist/UserBundle/Resources/views/Giftbox/shoppinglist.html.php
@@ -3,10 +3,5 @@ echo "<ul id='shoppinglistItems' class='contentItem'>\n";
 
 echo $view['actions']->render('WishlistListBundle:Shoppinglist:shoppinglist', array('userId' => $userId));
 
-//foreach ($purchases as $purchase)
-//{
-//    echo "<li>".$purchase->getItem()->getName()."</li>\n";
-//}
-
 echo "</ul>";
 ?>
