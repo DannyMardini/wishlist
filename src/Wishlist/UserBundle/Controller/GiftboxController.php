1890b77b996325d04ae299693df1e1ade0b45127
Andrea <andreacoba@gmail.com>
2013-05-04 15:11:26 -0700

    removed unused code

--- src/Wishlist/UserBundle/Controller/GiftboxController.php
+++ src/Wishlist/UserBundle/Controller/GiftboxController.php
@@ -22,34 +22,7 @@ class GiftboxController extends Controller
             return;
         }
         
-//        return $this->render('WishlistUserBundle:Giftbox:wishlist.html.php', array('wishlistItems' => $wishlistItems));
         return $this->render('WishlistUserBundle:Giftbox:wishlist.html.php', array('user' => $user));
-    }
-    
-    public function shoppinglistAction()
-    {
-        $userId = $this->getLoggedInUserId();
-        
-        return $this->render('WishlistUserBundle:Giftbox:shoppinglist.html.php', array('userId' => $userId));
-    }
-    
-    public function eventsAction()
-    {
-        $user = $this->getLoggedInUser();
-        
-        $events = $user->getEvents();
-        
-        return $this->render('WishlistUserBundle:Giftbox:events.html.php', array('events' => $events));
-    }
-    
-    public function friendsAction()
-    {
-        $userRepo = $this->getDoctrine()->getEntityManager()->getRepository('WishlistCoreBundle:WishlistUser');        
-        $user = $this->getLoggedInUser();
-        
-        $friends = $userRepo->getFriendsOf($user);
-                
-        return $this->render('WishlistUserBundle:Giftbox:friends.html.php', array('friends' => $friends));
     }
     
     private function getLoggedInUserId()
