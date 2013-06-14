339f19cb05d111cab5a7f7ee460e5bee07c2d062
Daniel Mardini <dannymardini@gmail.com>
2013-06-14 13:34:05 -0700

    Fixing a capitalization issue.

--- src/Wishlist/UserBundle/Resources/views/Default/friendpage.html.php
+++ src/Wishlist/UserBundle/Resources/views/Default/friendpage.html.php
@@ -1,5 +1,5 @@
 <!-- TODO: This page must not be viewable to those that aren't currently logged in. -->
-<?php $view->extend('::navbar.html.php') ?>
+<?php $view->extend('::navBar.html.php') ?>
 
 <script src="<?php echo $view['assets']->getUrl('bundles/fosjsrouting/js/router.js') ?>" type="text/javascript"></script>
 <script src="<?php echo $view['router']->generate('fos_js_routing_js', array("callback" => "fos.Router.setData")) ?>"></script>
