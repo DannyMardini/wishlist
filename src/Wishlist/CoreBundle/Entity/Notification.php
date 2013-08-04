6a63094a415c396662740a618ebb99f2e691d75c
Daniel Mardini <dannymardini@gmail.com>
2013-08-03 19:48:18 -0700

    Adding notification entity.

---
+++ src/Wishlist/CoreBundle/Entity/Notification.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Wishlist\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Wishlist\CoreBundle\Entity\Notification
+ */
+class Notification
+{
+    const STATE_UNREAD = 0;
+    const STATE_READ = 1;
+
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @var integer $state
+     */
+    private $state;
+
+    /**
+     * @var string $text
+     */
+    private $text;
+
+
+    /**
+     * Set state
+     *
+     * @param integer $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * Get state
+     *
+     * @return integer 
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+    /**
+     * @var Wishlist\CoreBundle\Entity\WishlistUser
+     */
+    private $wishlistUser;
+
+
+    /**
+     * Set wishlistUser
+     *
+     * @param Wishlist\CoreBundle\Entity\WishlistUser $wishlistUser
+     */
+    public function setWishlistUser(\Wishlist\CoreBundle\Entity\WishlistUser $wishlistUser)
+    {
+        $this->wishlistUser = $wishlistUser;
+    }
+
+    /**
+     * Get wishlistUser
+     *
+     * @return Wishlist\CoreBundle\Entity\WishlistUser 
+     */
+    public function getWishlistUser()
+    {
+        return $this->wishlistUser;
+    }
+}
