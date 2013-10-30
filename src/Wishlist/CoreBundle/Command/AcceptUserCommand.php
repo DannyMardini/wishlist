f664920fa8c7d43b606cf2df311beb7a93db95cf
Daniel Mardini <dannymardini@gmail.com>
2013-10-29 23:47:04 -0700

    Adding beginnings of wishenda console commands.

---
+++ src/Wishlist/CoreBundle/Command/AcceptUserCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wishlist\CoreBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AcceptUserCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('wishenda:acceptuser')
+            ->setDescription('Accept a user who has requested an invite')
+            ->addArgument('email', InputArgument::REQUIRED, 'Email address of user to accept');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = $input->getArgument('email');
+        $requestRepo = $this->getContainer()->get('doctrine')->getRepository('WishlistCoreBundle:Request');
+
+        $request = $requestRepo->findOnByEmail($email);
+
+        $output->writeln('This is where we would invite '.$request->getId());
+    }
+}
