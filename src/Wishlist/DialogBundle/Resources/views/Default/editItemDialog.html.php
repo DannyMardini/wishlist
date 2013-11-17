72ec62b9c492e7690d1eb11de75bdcb672747390
Daniel Mardini <dannymardini@gmail.com>
2013-11-17 13:02:31 -0800

    Hiding private functionality, add this later.

--- src/Wishlist/DialogBundle/Resources/views/Default/editItemDialog.html.php
+++ src/Wishlist/DialogBundle/Resources/views/Default/editItemDialog.html.php
@@ -5,7 +5,5 @@
     Link:  <input disabled type='text' id='link' placeholder='Enter Link' />
     Quantity: <input type='text' id='quantity' placeholder='Quantity (Default = 1)'/>
     Notes: <input type='text' id='notes' placeholder='Notes (Optional)'/>    
-    <span style='display:inline-block;'>Keep this wish private:</span>
-    <input style='width:25%;display:inline-block;' type='checkbox' id='isPrivate' />
     </span>
 </div>
