227612ba0e6630085db168d2ad0db3fe5510add3
Andrea <andreacoba@gmail.com>
2013-11-17 22:34:03 -0800

    changes so far for the reset password logic

--- src/Wishlist/CoreBundle/Resources/views/Default/resetpassword.html.php
+++ src/Wishlist/CoreBundle/Resources/views/Default/resetpassword.html.php
@@ -18,3 +18,36 @@
         </form>
     </body>
 </html>
+
+<script>
+    $(document).ready(function(){
+        $("#reset-password").submit(function(e){
+            e.preventDefault();
+            var url = $(this).attr('action');            
+
+            $.post( url, {email: $("#email").val()}, function(response){            
+
+                $dataArray = response.split(":"); 
+
+                if($dataArray[0].toLowerCase() == "success")
+                {
+                    alert('Message sent.');
+                }
+                else
+                {
+                    alert(response);
+                }
+                
+                redirectToQAHome();
+            });
+        });        
+    });
+    
+    
+    function redirectToQAHome()
+    {
+        window.location = Routing.generate('WishlistFrontpageBundle_homepage');
+    }
+    
+    
+</script>
--- src/Wishlist/QABundle/Controller/DefaultController.php
+++ src/Wishlist/QABundle/Controller/DefaultController.php
@@ -27,8 +27,47 @@ class DefaultController extends Controller
         return $this->render('WishlistQABundle:Default:ContactSupport.html.php');
     }
     
-    public function sendResetPasswordEmail()
+    public function resetPasswordRequestAction()
     {
-        return $this->render('WishlistQABundle:Default:ContactSupport.html.php');
-    }    
+        try
+        {
+            $response = "";
+            $email = $this->getRequest()->get('email');
+            $link = "";
+            
+            if(!$email)
+            {
+                $response = "The email was not filled in, please fill it in correctly. -Wishlist Team";
+            }
+            else
+            {
+                $userRepo = $this->getDoctrine()->getEntityManager()->getRepository('WishlistCoreBundle:WishlistUser');
+                $user = $userRepo->getUserWithEmail($email);
+                
+                if(!isset($user))
+                {
+                    return new Response ('The email does not pertain to a user. Please retry with the correct email.');
+                }
+                
+                $full_message = 'Hello ' . $user->getName() . ' We received a request to reset your password. ' . 
+                            ' If you did not submit this request please contact our Support Team immediately by emailing wishendasupport@wishnda.com ' .
+                            ' Otherwise please click the following link to reset your password: ' . $link;
+                $this->get("Mailer_Service")->sendMail("wishthrowaway@gmail.com", "User Inquiry", $full_message, $full_message);
+                $response = 'success';
+                
+                // the link has to pass in the user ID so that we can know what user is changing their password
+                // also set a flag in the DB to let us know that the user is waiting to change their password
+                // only change it if this flag is set in the DB, else don't do anything.
+                
+                
+            }
+            
+            return new Response($response);
+        }
+        catch(Exception $e)
+        {
+            $response = "Sorry about this! An issue occurred while sending the message. Please refresh your browser and try again. <br /><br />-Wishlist Team";
+            return $this->renderText($response);
+        }        
+    }
 }
