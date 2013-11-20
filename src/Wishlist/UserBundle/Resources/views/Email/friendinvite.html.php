5928a5d889c5df9c0c3f248c6d68467d1cdd3541
Daniel Mardini <dannymardini@gmail.com>
2013-11-19 20:17:56 -0800

    Adding new account link in emails.

--- src/Wishlist/UserBundle/Resources/views/Email/friendinvite.html.php
+++ src/Wishlist/UserBundle/Resources/views/Email/friendinvite.html.php
@@ -2,6 +2,6 @@
     <body style='font-family: helvetica,sans-serif; color: #666666'>
         <p><span style='font-weight: bold; color: #333333'><?php echo $name ?></span> wants to give you a gift but doesn't know what to get you, help them shop for you!</p>
 
-        <p><a href='#' style='text-decoration: none; font-weight: bolder'>Join Wishenda</a> and get your shopping done effortlessly! With Wishenda you know what all of your friends want and they know what you want, shopping for each other has never been easier!</p>
+        <p><a href='<?php echo $view['router']->generate('WishlistUserBundle_newAccountFriend', array('acceptId' => $acceptId)) ?>' style='text-decoration: none; font-weight: bolder'>Join Wishenda</a> and get your shopping done effortlessly! With Wishenda you know what all of your friends want and they know what you want, shopping for each other has never been easier!</p>
     </body>
 </html>
