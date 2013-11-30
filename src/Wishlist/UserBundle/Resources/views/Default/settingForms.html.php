3be1a7d84c96690fe2915b50fdb7e698302d0982
Andrea <andreacoba@gmail.com>
2013-11-29 20:13:38 -0800

    styled the section in the new account settings page for the terms and policy to be centered

--- src/Wishlist/UserBundle/Resources/views/Default/settingForms.html.php
+++ src/Wishlist/UserBundle/Resources/views/Default/settingForms.html.php
@@ -57,7 +57,7 @@ if(isset($birthdate))
 </form>
 <?php } ?>
 
-<label class='mainLabel'>By creating an account, I accept Wishlist's <a target="_blank" href="<?php echo $view['router']->generate('WishlistCoreBundle_Terms') ?>">Terms of Service</a> and <a target="_blank" href="<?php echo $view['router']->generate('WishlistCoreBundle_PrivacyPolicy') ?>">Privacy Policy</a>.</label>
+<label class="termsAndpolicy">By creating an account, I accept Wishlist's <a target="_blank" href="<?php echo $view['router']->generate('WishlistCoreBundle_Terms') ?>">Terms of Service</a> and <a target="_blank" href="<?php echo $view['router']->generate('WishlistCoreBundle_PrivacyPolicy') ?>">Privacy Policy</a>.</label>
 
 </div>
 <button id="saveChanges"><?php echo (isset($name) ? 'Save' : 'Create') ?></button>
