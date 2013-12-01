26b420d567f42a945a5bc890d2aae7b3ff6ab9e8
Andrea <andreacoba@gmail.com>
2013-12-01 15:31:48 -0800

    updated the text for the item dialog templates so that the Name is changed to Item Name so it's more specific and easier for our users to understand what to do

--- src/Wishlist/DialogBundle/Resources/views/Default/showFriendsWishItem.html.php
+++ src/Wishlist/DialogBundle/Resources/views/Default/showFriendsWishItem.html.php
@@ -1,6 +1,6 @@
 <div id='itemDialog' title='Wish' >  
     <input type='hidden' id='itemId' /> 
-    Name: <input disabled type='text' id='name' placeholder='Enter Name' /> 
-    Price: <input disabled type='text' id='price' placeholder='Enter Price' />
+    Item: <input disabled type='text' id='name' placeholder='Enter Item Name' /> 
+    Price: <input disabled type='text' id='price' placeholder='Enter Item Price' />
     Link: <div id='link2'></div> <input type='hidden' id='link' />
 </div>
