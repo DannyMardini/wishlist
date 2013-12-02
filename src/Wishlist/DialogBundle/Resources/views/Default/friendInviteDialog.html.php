82a23cc6875502e3cdb9ab8803a43b54a84ffdce
Andrea <andreacoba@gmail.com>
2013-12-01 16:44:14 -0800

    adding the missing dialog template

---
+++ src/Wishlist/DialogBundle/Resources/views/Default/friendInviteDialog.html.php
@@ -0,0 +1,11 @@
+
+
+<div id="friendInviteDialog" title="Invite a friend!">
+    <form id="friendInviteForm">
+        <div class="message narrow-box">Need to shop for a friend? Invite them to create their wishlist!</div>
+        <div class="inviteFriendInput">
+            <label class="form-label">Email:</label>
+            <input name="email" id="newFriendEmail" type="email" placeholder="johndoe@email.com"></div>
+        <input id="friendInviteFormSubmit" type="submit" name="submitFriend" style="display: none" required>
+    </form>
+</div>
