1196f015f2c75cdd6a1698d406867ae82a549a62
Daniel Mardini <dannymardini@gmail.com>
2013-12-11 23:07:17 -0800

    Fixing capitalization issue.

--- src/Wishlist/DialogBundle/Resources/views/Default/amazonSearchDialog.html.php
+++ src/Wishlist/DialogBundle/Resources/views/Default/amazonSearchDialog.html.php
@@ -1,6 +1,6 @@
 <div id='amazonSearchDialog' title='Amazon Search' >  
     <input type='hidden' id='itemId' /> 
-    <span class="subtext">(hit Enter to search amazon)</span>
+    <span class="subtext">(Hit enter to search amazon)</span>
     <input type='text' id='name' placeholder='Item Name' />
     <span id='resultsArea' class='message'></span>
     <span id='otherItem' class='subtext'><a>Can't find what you want? Click here to create your own item</a></span>
