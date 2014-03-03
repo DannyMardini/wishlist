dc2128443383a9e1fc0225a5703f2ff3f6be879f
Andrea <andreacoba@gmail.com>
2014-03-02 18:14:17 -0800

    modified the text in the amazon search box

--- src/Wishlist/DialogBundle/Resources/views/Default/amazonSearchDialog.html.php
+++ src/Wishlist/DialogBundle/Resources/views/Default/amazonSearchDialog.html.php
@@ -3,5 +3,5 @@
     <span class="subtext">(Hit enter to search amazon)</span>
     <input type='text' id='name' placeholder='Item Name' />
     <span id='resultsArea' class='message'></span>
-    <span id='otherItem' class='subtext'><a>Can't find what you want? Click here to create your own item</a></span>
+    <span id='otherItem' class='subtext'><a>Can't find what you want? Click here</a></span>
 </div>
