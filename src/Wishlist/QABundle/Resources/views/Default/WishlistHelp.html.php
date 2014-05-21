799e15c84a282de9e4e63b5548d20b697c15bd94
Andrea <andreacoba@gmail.com>
2014-05-20 23:04:16 -0700

    working on fixing the look of the help section

--- src/Wishlist/QABundle/Resources/views/Default/WishlistHelp.html.php
+++ src/Wishlist/QABundle/Resources/views/Default/WishlistHelp.html.php
@@ -1,20 +1,35 @@
-<div>The wish list is used to track items that you would like your family and friends to buy for you.
-Add items to this list so that your friends know exactly what you want for your birthday, christmas, etc.</div>
-
-<div class="contentInfoTopic">Adding Wishes</div>
-<div class="contentInfoDetails">
-    1. To add a new item to your wishlist, click on the plus icon next to the Wishlist header. <br /><br />
-    2. You can also add an item to your list by clicking on an item that your friend has on their wish list and clicking 'Want it'<br />
-</div>
-<div class="contentInfoTopic">Removing Wishes</div>
-<div class="contentInfoDetails">
-    To remove an item from your wishlist, select the item in your list and click 'Delete' <br />
-</div>                
-<div class="contentInfoTopic">Granting a Wish</div>
-<div class="contentInfoDetails">
-    1. To grant a wish for one of your friends, simply go to their wishlist and select an item; then click 'Grant'. <br /><br />
-    2. You may also grant a wish for yourself by selecting an item from your own wishlist and clicking 'Grant'.<br /><br />
+<div class='jumbotron'>
+    <h1><small>Learn about the Wish List:</small></h1><br />
+    <h3><small>Show friends what you want by adding items to your wish list</small></h3><br />
     
-    When you grant a wish, it asks you to select a date or event. This date signifies the day on which the item will pop off your shopping list. It is the day that you have promised to deliver the gift to your friend by. <br />
+    <h4>Add an item</h4>
+    <h3><small>Add an item from an online store (amazon, best buy, more to come)</small></h3>
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Click on the <i><b>+</b></i> icon next to the Wishlist header <br />
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Search for an item by selecting a store to search in and typing the item name.<br />
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Browse through the available items, click on an item to select it.<br />
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Click on an items image to see more details about that item.<br />
+    <h3><small>Can't find what you want from the stores? Create your own item!</small></h3>
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Click on the <i><b>+</b></i> icon next to the Wishlist header <br />
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Click <i>Create New Item</i>.<br />
+   <h3><small>See something you want on a friends wish list? Add it to your wish list!</small></h3>
+        <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+        Click on the item that your friend has on their wish list and click <i>Want it</i>
+   <h4>Remove an item</h4>
+   <h3><small>Changed your mind about an item on your wish list?</small></h3>
+   <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+   To remove an item from your wish list, select the item in your list and click <i>Delete</i><br />
+   <h4>Grant a wish</h4>
+   <h3><small>If you'd like to give your friend a gift or give yourself a gift...</small></h3>
+   <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+   Simply go to their wish list and select an item; then click <i><b>Grant</b></i><br />
+   <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+   To grant a wish to yourself, select the item on your wish list and click <i><b>Grant</b></i><br />
+   <span class='bullet-icon ui-icon ui-icon-carat-1-e'></span>
+   Note that, when granting a wish, you must select a date or event. This signifies when you plan to purchase the item by and when you promise to deliver the gift to your friend by<br />   
 </div>
-
